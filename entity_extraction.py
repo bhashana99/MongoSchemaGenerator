0e02b4b4993fd51eff50b1f5f2257488eedabc82
bhashana99 <bhashanachamodya99@gmail.com>
2025-10-29 19:44:29 +0530

    Added entity extraction script using spaCy for identifying entities and attributes from natural language requirements

---
+++ entity_extraction.py
@@ -0,0 +1,37 @@
+import spacy
+
+
+nlp = spacy.load("en_core_web_sm")
+
+
+text = "A student has a name and ID."
+
+
+doc = nlp(text)
+
+entities = {}
+
+for sent in doc.sents:
+    words = [token.text for token in sent]
+
+    
+    if "has" in words:
+        has_index = words.index("has")
+
+        if has_index > 0:
+            entity = words[has_index - 1].capitalize()
+        else:
+            entity = words[0].capitalize()
+
+        attributes_text = " ".join(words[has_index + 1:]).replace(".", "")
+        attributes_text = attributes_text.replace("and", ",")  
+        attributes = [attr.strip() for attr in attributes_text.split(",") if attr.strip()]
+
+        clean_attrs = []
+        for attr in attributes:
+            attr = attr.replace("a ", "").replace("an ", "").replace("list of ", "").strip()
+            clean_attrs.append(attr)
+
+        entities[entity] = clean_attrs
+
+print(entities)
